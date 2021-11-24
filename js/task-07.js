667d8199237fae8223f13a9760b09199c42df5c7
ed_bolma <ed.bolma21@gmail.com>
2021-11-24 15:54:07 +0100

    task 07 is completed

---
+++ js/task-07.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#font-size-control');
+// console.dir(input.value);
+const text = document.querySelector('#text');
+// console.log(text);
+
+text.style.fontSize = `${input.valueAsNumber}px`;
+
+const onSlideChange = (event) => {
+    
+    console.log(`${event.currentTarget.valueAsNumber}px`);
+    text.style.fontSize = `${event.currentTarget.valueAsNumber}px`;
+}
+
+input.addEventListener('input', onSlideChange);

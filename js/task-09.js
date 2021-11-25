bbd16bdd020460f57436c8c70b4fb99b4ee07796
ed_bolma <ed.bolma21@gmail.com>
2021-11-25 15:32:55 +0100

    Task-09 is completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ChangeColorBtn = document.querySelector('.change-color');
+// console.log(ChangeColorBtn);
+let colorName = document.querySelector('.color');
+// console.log(colorName.textContent);
+const widget = document.querySelector('.widget');
+// console.log(widget);
+
+
+const onClickChangeColor = () => {
+  colorName.textContent = getRandomHexColor();
+  widget.style.backgroundColor = getRandomHexColor();
+}
+
+ChangeColorBtn.addEventListener('click', onClickChangeColor);
